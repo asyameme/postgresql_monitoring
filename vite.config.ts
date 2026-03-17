fa40410924cfe68d2725b6b6bd73b40a4be401f1
Анастасия Харламова <asya@MacBook-Air-Anastasia.local>
2026-03-17 15:34:53 +0300

    Add GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': './src',
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/postgresql_monitoring/' : '/',
+  base: mode === 'production' ? '/postgresql_monitoring/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
-})
+}))
